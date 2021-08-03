9fb4c780702c3986c5e3968eb6a8ca662b650732
Hadi Ahmed <hadiahmed098@gmail.com>
2021-08-02 21:39:59 -0500

    Added Vue - Helps to emulate PHP with a header/footer and auto changing year/page title/header

---
+++ assets/include.js
@@ -0,0 +1,100 @@
+// Header and Navbar
+Vue.component('ieee-header', {
+    mounted: function() {
+        document.title = `${this.page} | Purdue IEEE`;
+    },
+    props: ['head', 'page'],
+    template:`
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-lg">
+          <a class="navbar-brand d-flex me-auto w-50" href="/"><img class="me-2" src="/images/IEEE-Kite.svg" alt="" width="30" height="30"> <span class="fw-bold">{{head}}</span></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarlink" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <!-- navbarlinks -->
+          <div class="collapse navbar-collapse" id="navbarlink">
+            <ul class="navbar-nav ms-auto mb-2">
+                <!-- navbarlinks_about -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarlinks_about" role="button" data-bs-toggle="dropdown" aria-expanded="false">About</a>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarlinks_about">
+                    <li><a class="dropdown-item" href="#">About IEEE</a></li>
+                    <li><a class="dropdown-item" href="#">Officers</a></li>
+                    <li><a class="dropdown-item" href="#">Annual Award Winners</a></li>
+                    <li><a class="dropdown-item" href="#">Students of the Week</a></li>
+                    <li><a class="dropdown-item" href="#">Constitution</a></li>
+                </ul>
+              </li>
+              <!-- /navbarlinks_about -->
+               <li class="nav-item">
+                <a class="nav-link" href="#">Calender</a>
+              </li>
+              <!-- navbarlinks_committees -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarlinks_committees" role="button" data-bs-toggle="dropdown" aria-expanded="false">Committees</a>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarlinks_committees">
+                    <li><div class="dropdown-header py-1">Cornerstone</div></li>
+                    <li><a class="dropdown-item" href="#">Learning</a></li>
+                    <li><a class="dropdown-item" href="#">Industrial Relations</a></li>
+                    <li><a class="dropdown-item" href="#">Social</a></li>
+                    <li><a class="dropdown-item" href="#">Growth & Engagement</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><div class="dropdown-header py-1">Technical</div></li>
+                    <li><a class="dropdown-item" href="#">Aerial Robotics</a></li>
+                    <li><a class="dropdown-item" href="#">Computer Society</a></li>
+                    <li><a class="dropdown-item" href="#">EMBS</a></li>
+                    <li><a class="dropdown-item" href="#">MTT-S</a></li>
+                    <li><a class="dropdown-item" href="#">Racing</a></li>
+                    <li><a class="dropdown-item" href="#">ROV</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><div class="dropdown-header py-1">Events</div></li>
+                    <li><a class="dropdown-item" href="#">Software Saturdays</a></li>
+                </ul>
+              </li>
+              <!-- /navbarlinks_committees -->
+              <li class="nav-item">
+                <a class="nav-link" href="#">Join</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /navbarlinks -->
+        </div>
+      </nav>
+      <!-- /navbar -->
+    `
+});
+
+Vue.component('ieee-footer',{
+    data: function() {
+        return {
+            year: (new Date()).getFullYear(),
+        }
+    },
+    props: [],
+    template: `
+    <footer>
+        <!-- footer -->
+        <div class="container-fluid mt-5 text-center py-3">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                <a href="https://twitter.com/purdueieee" target="_blank" class="link-dark"><i class="bi bi-twitter fs-1 mx-2" width="50"></i></a>
+                <a href="https://www.facebook.com/groups/purdueieee/" target="_blank" class="link-dark"><i class="bi bi-facebook fs-1 mx-2"></i></a>
+                <a href="https://www.youtube.com/channel/UCJwFwlur0e2fgwf97eV7INw" target="_blank" class="link-dark"><i class="bi bi-youtube fs-1 mx-2"></i></a>
+                <a href="https://discord.gg/kZNkKXM" target="_blank" class="link-dark"><i class="bi bi-discord fs-1 mx-2"></i></a>
+                <a href="https://purdueieee.slack.com/signup" target="_blank" class="link-dark"><i class="bi bi-slack fs-1 mx-2"></i></a>
+                </div>
+            </div>
+            <p class="text-center fs-5">Copyright © {{this.year}} Purdue IEEE</p>
+        </div>
+        <!-- /footer -->
+    </footer>
+    `
+})
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+ 
+    },
+})
