44cf939d3dcf3d6d51d3a70d28cf9418f9bb481b
Hadi Ahmed <hadiahmed098@gmail.com>
2021-08-29 16:48:03 -0400

    Need to test on prod server, this code may be bad

--- assets/join.js
+++ assets/join.js
@@ -7,7 +7,25 @@ const vm = new Vue({
     methods: {
       submitEmail: function() {
         // This will need to be added...
-        //this.mailinglistSubmit=true;
+        lists = ['ieee-announcements']
+
+        fetch('https://purdueieee.org/DirectoryServices/directories.php', {
+          method: 'post',
+          mode: 'no-cors',
+          headers: new Headers({'content-type': 'application/x-www-form-urlencoded; charset=UTF-8','Accept': '*/*',}),
+          body: encodeURI(`email=${this.mailinglistEmail}&list[]=ieee-announcements`).replace('@', '%40'),
+        })
+        .then((response) => {
+          return response.text();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
+        this.mailinglistSubmit=true;
       }
     }
 });
