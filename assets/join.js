3a655ef1155f8d221fa55da99f13aaaae970ea18
Hadi Ahmed <hadiahmed098@gmail.com>
2021-08-29 17:10:59 -0400

    CORS attempt 4 #24

--- assets/join.js
+++ assets/join.js
@@ -11,7 +11,6 @@ const vm = new Vue({
 
         fetch('https://purdueieee.org/DirectoryServices/directories.php', {
           method: 'post',
-          mode: 'no-cors',
           headers: new Headers({'content-type': 'application/x-www-form-urlencoded; charset=UTF-8','Accept': '*/*',}),
           body: encodeURI(`email=${this.mailinglistEmail}&list[]=ieee-announcements`).replace('@', '%40'),
         })
