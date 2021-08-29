36ef8c9061ed12a29edeb76401379e981676f668
Hadi Ahmed <hadiahmed098@gmail.com>
2021-08-29 17:45:11 -0400

    Mailing lists done, only has announcements now #24

--- assets/join.js
+++ assets/join.js
@@ -2,29 +2,35 @@ const vm = new Vue({
     el: '#app',
     data: {
       mailinglistEmail: "",
-      mailinglistSubmit: false,
+      mailinglistSubmitGood: false,
+      mailinglistSubmitGoodMsg: "",
+      mailinglistSubmitFail: false,
+      mailinglistSubmitFailMsg: "",
     },
     methods: {
       submitEmail: function() {
-        // This will need to be added...
         lists = ['ieee-announcements']
 
+        // CORS sucks and this may break at any time...
         fetch('https://purdueieee.org/DirectoryServices/directories.php', {
           method: 'post',
           headers: new Headers({'content-type': 'application/x-www-form-urlencoded; charset=UTF-8','Accept': '*/*',}),
-          body: encodeURI(`email=${this.mailinglistEmail}&list[]=ieee-announcements`).replace('@', '%40'),
+          body: encodeURI(`email=${this.mailinglistEmail}&list[]=ieee-announcements`).replace('@', '%40'), // properly encode form data
         })
         .then((response) => {
-          return response.text();
+          return response.text(); // Endpoint returns HTML
         })
         .then((data) => {
-          console.log(data);
+          // We are ignoring the HTML returned by the site but this can be changed 
+          //   in the ~~future~~ if we reimplement the other lists besides IEEE-A
+          this.mailinglistSubmitGoodMsg = "You have been subscribed to IEEE-Announcements!\nCheck your email to confirm.";
+          this.mailinglistSubmitGood = true;
         })
         .catch((error) => {
           console.log(error);
+          this.mailinglistSubmitFailMsg = "Error submitting form: please contact webmaster!";
+          this.mailinglistSubmitFail = true;
         })
-
-        this.mailinglistSubmit=true;
       }
     }
 });
