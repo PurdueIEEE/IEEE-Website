b63abcc5448956323103e07de18bd1dcc417a4dd
Hadi Ahmed <hadiahmed098@gmail.com>
2022-02-15 22:30:46 -0500

    Added learning dates

--- assets/include.js
+++ assets/include.js
@@ -96,10 +96,14 @@ Vue.component('ieee-footer',{
 });
 
 // Google Analytics Tracker
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+// only send on actual deployment
+if (location.hostname === "purdueieee.org") {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-66463204-1', 'auto');
-ga('send', 'pageview');
+  ga('create', 'UA-66463204-1', 'auto');
+  ga('send', 'pageview');
+  console.log("GA logged");
+}
