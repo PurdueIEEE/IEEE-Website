f413e89c6f901c265b5ec5e947b4faaaa0e1e010
Hadi Ahmed <hadiahmed098@gmail.com>
2022-08-23 15:22:24 -0400

    Added callout and info session times

--- assets/callout.js
+++ assets/callout.js
@@ -4,18 +4,15 @@ let date = new Date();
 let date_i = new Date();
 
 let info_sessions = [
-    {date:{month:1,day:19},time:{hour:18,minute:30},committee:'Purdue IEEE',location:'BHEE 129'},
-    {date:{month:1,day:24},time:{hour:18,minute:00},committee:'Aerial Robotics',location:'ARMS 1010'},
-    {date:{month:1,day:25},time:{hour:18,minute:30},committee:'EMBS',location:'BHEE 224'},
-    {date:{month:1,day:26},time:{hour:18,minute:30},committee:'Racing',location:'<a href="https://discord.gg/qUmscXNQBU">IEEE Racing Discord</a>'},
-    {date:{month:1,day:26},time:{hour:18,minute:30},committee:'ROV',location:'POTR 234'},
-    {date:{month:1,day:26},time:{hour:19,minute:00},committee:'MTT-S',location:'<a href="https://discord.gg/kZNkKXM">IEEE Q&amp;A Discord [MTT-S]</a>'},
-    {date:{month:1,day:27},time:{hour:20,minute:00},committee:'Computer Society',location:'BHEE 222'},
-    {date:{month:1,day:29},time:{hour:15,minute:30},committee:'Software Saturdays',location:'<a href="https://purdue-edu.zoom.us/j/98093022022?pwd=OUpsUEg5YUVzeEFHUmluSXhEd3VkQT09">HAMP 1144 and Zoom</a>'},
-    /*{date:{month: 9,day: 14},time:{hour: 18,minute: 30},committee:'Learning',location:'PHYS 201'},
-    {date:{month: 9,day: 14},time:{hour: 18,minute: 30},committee:'Social',location:'PHYS 201'},
-    {date:{month: 9,day: 14},time:{hour: 18,minute: 30},committee:'Industrial Relations',location:'PHYS 201'},
-    {date:{month: 9,day: 14},time:{hour: 18,minute: 30},committee:'Growth &amp; Engagement',location:'PHYS 201'},*/
+    {date:{month:8,day:30},time:{hour:18,minute:30},committee:'Purdue IEEE',location:'BHEE 129'},
+    {date:{month:8,day:31},time:{hour:18,minute:30},committee:'Aerial Robotics (Session 1)',location:'ARMS B061'},
+    {date:{month:8,day:31},time:{hour:18,minute:30},committee:'ROV',location:'POTR 234, EE 020'},
+    {date:{month:9,day:01},time:{hour:18,minute:30},committee:'Aerial Robotics (Session 2)',location:'ARMS B071'},
+    {date:{month:9,day:07},time:{hour:18,minute:30},committee:'EMBS',location:'BHEE 222'},
+    {date:{month:9,day:08},time:{hour:19,minute:30},committee:'Computer Society',location:'BHEE 222'},
+    /*{date:{month:8,day:26},time:{hour:18,minute:30},committee:'Racing',location:'<a href="https://discord.gg/qUmscXNQBU">IEEE Racing Discord</a>'},
+    {date:{month:8,day:26},time:{hour:19,minute:00},committee:'MTT-S',location:'<a href="https://discord.gg/kZNkKXM">IEEE Q&amp;A Discord [MTT-S]</a>'},
+    {date:{month:8,day:29},time:{hour:15,minute:30},committee:'Software Saturdays',location:'<a href="https://purdue-edu.zoom.us/j/98093022022?pwd=OUpsUEg5YUVzeEFHUmluSXhEd3VkQT09">HAMP 1144 and Zoom</a>'},*/
 ]
 
 let table = document.getElementById('infosessions_tbody');
@@ -28,13 +25,9 @@ for (session of info_sessions) {
     date_i.setMonth(session.date.month-1, session.date.day);
     row = `<tr ${ date <= date_i ? '' : 'class="text-decoration-line-through"'}>`;
 
-    if (prevdate == `${session.date.month}/${session.date.day.toString().padStart(2, "0")}`) {
-        row += `<th scope="row"></th>`;
-    } else {
-        prevdate = `${session.date.month}/${session.date.day.toString().padStart(2, "0")}`;
-        row += `<th scope="row">${prevdate}</th>`;
-    }
+    prevdate = `${session.date.month}/${session.date.day.toString().padStart(2, "0")}`;
 
+    row += `<th scope="row">${prevdate}</th>`;
     row += `<td>${session.committee}</td>`;
     row += `<td>${session.time.hour > 12 ? session.time.hour - 12 : session.time.hour}:${session.time.minute.toString().padStart(2, "0")} ${session.time.hour > 12 ? 'PM' : 'AM'}</td>`;
     row += `<td>${session.location}</td>`;
