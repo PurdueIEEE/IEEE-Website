b7cb7216b17393a906d837e23e48e6da3efa7509
Hadi Ahmed <hadiahmed098@gmail.com>
2022-09-16 00:38:00 -0400

    Migrate to new UA-4 tag

--- assets/include.js
+++ assets/include.js
@@ -101,9 +101,12 @@ if (location.hostname === "purdueieee.org") {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','//www.googletagmanager.com/gtag/js?id=G-E3YRT96GPT','gtag');
 
-  ga('create', 'UA-66463204-1', 'auto');
-  ga('send', 'pageview');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E3YRT96GPT')
   console.log("GA logged");
 }
