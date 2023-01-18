dbbfa73c8dc775fccbafcfed9455de6ec8afc4c1
Rmalavathu <44881314+Rmalavathu@users.noreply.github.com>
2023-01-18 15:31:33 -0500

    Updated Officers and info session details (#38)
    * Added the new VP for Technical Committees, Added info session information for Csociet and Racing, updated ROV join page
    
    * removed duplicated callout entries
    
    Co-authored-by: rmalavat <rmalavat@purdue.edu>

--- assets/callout.js
+++ assets/callout.js
@@ -9,22 +9,15 @@ let info_sessions = [
     {date:{month:1,day:18},time:{hour:18,minute:30},committee:'Purdue IEEE',location:'BHEE 129'},
     {date:{month:1,day:18},time:{hour:18,minute:30},committee:'Cornerstones',location:'BHEE 129'},
     {date:{month:1,day:19},time:{hour:18,minute:00},committee:'EMBS',location:'BHEE 226'},
+    {date:{month:1,day:19},time:{hour:19,minute:30},committee:'Computer Society',location:'BHEE 224'},
     {date:{month:1,day:21},time:{hour:13,minute:00},committee:'ROV',location:'POTR 234'},
+    {date:{month:1,day:23},time:{hour:17,minute:30},committee:'Racing',location:'BHEE 005'},
     {date:{month:1,day:23},time:{hour:18,minute:30},committee:'MTT-S',location:'BHEE 224'},
     {date:{month:1,day:28},time:{hour:11,minute:30},committee:'Software Saturdays',location:'WALC 1018'},
 
-
-
-
     /*
-    {date:{month:1,day:19},time:{hour:19,minute:30},committee:'Computer Society',location:'BHEE 129'},
-    {date:{month:9,day:06},time:{hour:17,minute:30},committee:'Racing',location:'BHEE 005'},
     {date:{month:9,day:12},time:{hour:17,minute:30},committee:'GRSS',location:'BHEE 005'},
-    
-
     */
-    /*{date:{month:8,day:26},time:{hour:18,minute:30},committee:'Racing',location:'<a href="https://discord.gg/qUmscXNQBU">IEEE Racing Discord</a>'},
-    {date:{month:8,day:29},time:{hour:15,minute:30},committee:'Software Saturdays',location:'<a href="https://purdue-edu.zoom.us/j/98093022022?pwd=OUpsUEg5YUVzeEFHUmluSXhEd3VkQT09">HAMP 1144 and Zoom</a>'},*/
 ]
 
 let table = document.getElementById('infosessions_tbody');
